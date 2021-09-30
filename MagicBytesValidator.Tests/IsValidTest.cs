6aba5a704b62a98bbb809f99111146857cc19859
Tobias Janssen <tobias.janssen@traperto.com>
2021-09-30 19:13:30 +0200

    test(core): IsValid Method Unittest

---
+++ MagicBytesValidator.Tests/IsValidTest.cs
@@ -0,0 +1,81 @@
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MagicBytesValidator.Formats;
+using MagicBytesValidator.Models;
+using MagicBytesValidator.Services;
+using Xunit;
+
+namespace MagicBytesValidator.Tests
+{
+    public class BasicTest
+    {
+        private readonly Validator _validator;
+        private readonly FileType _fileTypeGif;
+        private readonly FileType _fileTypePng;
+        private readonly MemoryStream _gifMemoryStream;
+        private readonly Gif _gif;
+        private readonly Png _png;
+
+        public BasicTest()
+        {
+            _gif = new Gif();
+            _png = new Png();
+            _gifMemoryStream = new MemoryStream();
+
+            _validator = new Validator();
+            _fileTypeGif = _validator.Mapping.FindByExtension(_gif.Extensions.First());
+            _fileTypePng = _validator.Mapping.FindByMimeType(_png.MimeType);
+        }
+
+        [Fact]
+        public async Task Should_validate()
+        {
+            await _gifMemoryStream.WriteAsync(_gif.MagicByteSequences.First());
+
+            // Act
+            var valid = await _validator.IsValidAsync(_gifMemoryStream, _fileTypeGif, CancellationToken.None);
+
+            // Assert
+            valid.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Should_fail_incorrect_extension()
+        {
+            await _gifMemoryStream.WriteAsync(_gif.MagicByteSequences.First());
+
+            // Act
+            var invalidExtension = await _validator.IsValidAsync(_gifMemoryStream, _fileTypePng, CancellationToken.None);
+
+            // Assert
+            invalidExtension.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Should_fail_incorrect_mimetype()
+        {
+            await _gifMemoryStream.WriteAsync(_gif.MagicByteSequences.First());
+
+            // Act
+            var inValidMimeType = await _validator.IsValidAsync(_gifMemoryStream, _fileTypePng, CancellationToken.None);
+
+            // Assert
+            inValidMimeType.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Should_fail_incorrect_magicByte_sequence()
+        {
+            await _gifMemoryStream.WriteAsync(_png.MagicByteSequences.First());
+
+            // Act
+            var invalidMagicByte = await _validator.IsValidAsync(_gifMemoryStream, _fileTypeGif, CancellationToken.None);
+
+            // Assert
+            invalidMagicByte.Should().BeFalse();
+        }
+    }
+}
